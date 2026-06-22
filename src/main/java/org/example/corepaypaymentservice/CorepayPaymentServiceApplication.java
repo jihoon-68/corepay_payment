d993e2d6860197d83f605438a7a2b7db3f7958fe
jihoon-68 <pkrwlgns64@gmail.com>
2026-06-22 19:15:40 +0900

    feat: PaymentEventHandler에 결제 취소 이벤트 처리 및 아웃박스 저장 기능 추가

--- src/main/java/org/example/corepaypaymentservice/CorepayPaymentServiceApplication.java
+++ src/main/java/org/example/corepaypaymentservice/CorepayPaymentServiceApplication.java
@@ -2,10 +2,20 @@ package org.example.corepaypaymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing
 @SpringBootApplication(scanBasePackages = {
+        "org.example.corepaypaymentservice",
+        "org.example.corepaycommon"
+})
+@EnableJpaRepositories(basePackages = {
+        "org.example.corepaypaymentservice",
+        "org.example.corepaycommon"
+})
+@EntityScan(basePackages = {
         "org.example.corepaypaymentservice",
         "org.example.corepaycommon"
 })
--- src/main/java/org/example/corepaypaymentservice/payment/application/event/PaymentEventHandler.java
+++ src/main/java/org/example/corepaypaymentservice/payment/application/event/PaymentEventHandler.java
@@ -1,9 +1,13 @@
 package org.example.corepaypaymentservice.payment.application.event;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.corepaypaymentservice.payment.infrastructure.kafka.PaymentEventProducer;
+import org.example.corepaycommon.outbox.OutboxEvent;
+import org.example.corepaycommon.outbox.OutboxRepository;
 import org.example.corepaypaymentservice.payment.infrastructure.kafka.event.PaymentRefundEvent;
+import org.slf4j.MDC;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -14,12 +18,32 @@ import org.springframework.transaction.event.TransactionalEventListener;
 @RequiredArgsConstructor
 public class PaymentEventHandler {
 
-    private final PaymentEventProducer producer;
+    private final ObjectMapper objectMapper;
+    private final OutboxRepository outboxRepository;
 
     // 3. 결제 취소 결과 오더 서버로 발송
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Async
     public void PaymentCancelEvent(PaymentRefundEvent event){
-        producer.sendPaymentCancelEvent(event);
+        saveOutbox("payment-refund-topic",event);
+    }
+
+    private void saveOutbox(String topic, Object event){
+        try {
+            String messagePayload = objectMapper.writeValueAsString(event);
+
+            String traceId = MDC.get("traceId");
+
+            outboxRepository.save(OutboxEvent.builder()
+                    .topic(topic)
+                    .payload(messagePayload)
+                    .traceId(traceId != null ? traceId : "UNKNOWN-TRACE")
+                    .build()
+            );
+            log.info("[Outbox 저장 완료] 토픽: {}", topic);
+        }catch (JsonProcessingException e){
+            log.error("Outbox 메시지 직렬화 에러. 토픽: {}", topic, e);
+        }
+
     }
 }
